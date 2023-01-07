ceab065416cff687d6963f90b6b6380b851ce3dc
@zwcrowley-qczvvO <zwcrowley@gmail.com>
2023-01-06 21:13:46 -0600

    drop down menu works, lots more to go

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -5,10 +5,21 @@ const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
 const dataPromise = d3.json(url);
 console.log("Data Promise: ", dataPromise);
 
-// Fetch the JSON data and console log it
+// function to create the drop down menu
+function createDropDown(data) {
+  // Get names as a var:
+  names = data.names;
+  // Select the id for the drop down menu and set as a var:
+  select = d3.select("#selDataset")
+  // Set up the dropdown menu by pulling the individual id and passing it to the select tag and creating an "option" child
+  // using forEach to iterate through the individual ids:
+  names.forEach((name) => {
+    select.append("option").text(name).property("value", name);
+  });
+}
+
+// Fetch the JSON data and run the following functions:
+//  createDropDown = for creating the dropDownMenu
 d3.json(url).then(function(data) {
-  console.log("bebo data:",data);
-});
-
-// 
-
+  createDropDown(data)
+});  
