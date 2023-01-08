fc27887de9c4e179d1ea76186d8953510899d6bd
@zwcrowley-qczvvO <zwcrowley@gmail.com>
2023-01-07 21:11:18 -0600

    Bar chart is working

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -11,62 +11,115 @@ console.log("Data Promise: ", dataPromise);
 // Fetch the JSON data and start to run the following functions to complete all of the dashboard requirements:
 d3.json(url).then(function(data) {
   
-
   // Function to create the drop down menu using "names" from the belly button JSON data:
   function createDropDown(data) {
     // Get names as a var:
-    let names = data.names;
-    console.log("Names Data: ",names) 
+    var names = data.names;
     // Select the id for the drop down menu and set as a var:
-    let select = d3.select("#selDataset");
+    var select = d3.select("#selDataset");
     // Set up the dropdown menu by pulling the individual id and passing it to the select tag and creating an "option" child using forEach to iterate through the individual ids:
     names.forEach((name) => {
       select.append("option").text(name).property("value", name);  
       });
   }
 
-  // 
+  // Function to initialize all the charts and panel with the first Id = 940:
   function init() {
     var firstName = data.names[0];
-    getmetaData(firstName); 
-    // buildBarChart(firstName);
+    getData(firstName); 
+    // makeBarChart(firstName);
 
   }
-  
   
   // On change to the DOM, call getmetaData()
-  d3.selectAll("#selDataset").on("change", getmetaData);
+  d3.selectAll("#selDataset").on("change", getData);
 
-  // 
-  function optionChanged(selectObject) {
-    getmetaData(selectObject)
-    console.log("selected: ", selectObject)
-  } 
-
-  // Function called by DOM changes:
-  function getmetaData() {
-    // let dropdownMenu = d3.select("#selDataset");
+  // Function called by onchange in the drop down Menu, this gets the data from the chosen ID for the metadata for the panel demo table and the sample data for the charts:
+  function getData() {
     // Assign the value of the dropdown menu option to a variable:
-    let dataValue = d3.select("#selDataset").property("value");
-    console.log("Chosen value: ", dataValue)  
+    var dataValue = d3.select("#selDataset").property("value");
 
     // Set metadata to a var:
-    let metadata = data.metadata;
+    var metadata = data.metadata;
     // Filter the metadata to find the id that was selected in the dropdownMenu (dataValue)):
-    let chosenMetadata = metadata.filter(sample => sample.id == dataValue);  
+    var chosenMetadata = metadata.filter(sample => sample.id == dataValue);  
     console.log("Result Array: ",chosenMetadata) 
-    let chosenMeta = chosenMetadata[0] 
-    
-    // Call function to update the panel:
+    var chosenMeta = chosenMetadata[0] 
+
+    // Call "updatePanel" function to update the panel with the new metadata:
     updatePanel(chosenMeta);
+
+    // Get data for the bar chart and the bubble chart:
+    // Set samples to a var:
+    var samples = data.samples;
+    // Filter the samples to match the chosen value in the dropdown menu= dataValue, add [0] to the end to pull out that sample key:
+    var sampleMatched = samples.filter(s => s.id.toString() === dataValue)[0];
+
+    // Call "makeBarChart" function to pass the sampleMatched to it: 
+    makeBarChart(sampleMatched); 
+    // makeBubbleChart(sampleMatched);
   }
 
-  // Update the restyled plot's values
+  // Function to make the horz bar chart:
+  function makeBarChart(newdata) {
+    // Set all the vars in the array of sampleMatched (newdata) to object vars to build the charts, slice the top 10 and reverse them:
+    var otu_ids = newdata.otu_ids.slice(0, 10).reverse();
+    var otu_labels = newdata.otu_labels.slice(0, 10).reverse();
+    var sample_values = newdata.sample_values.slice(0, 10).reverse();
+
+    // console.log("Chosen otu_ids: ", otu_ids);
+    // console.log("Chosen otu_labels: ", otu_labels);
+    // console.log("Chosen otu_values: ", sample_values); 
+
+    // Re-format the otu_ids as labels for the y-axis:
+    var y_labels = otu_ids.map(otu_id => `OTU ${otu_id}`);   
+    // console.log("Chosen y_labels: ", y_labels);   
+
+    // Trace1 for the top 10 belly button data: 
+    var trace1 = {
+      x: sample_values,
+      y: y_labels, 
+      text: otu_ids,
+      hovertext: otu_labels,
+      name: "beboBar",
+      type: "bar",
+      orientation: "h"
+    };
+
+    // add the trace1 to a barData array:
+    var barData = [trace1];
+
+    // Apply a title to the layout and margins, pull the ID for the title:
+    let layout = {
+      title: `Top Ten OTUs in ID: ${newdata.id}`, 
+      margin: {
+        l: 100,
+        r: 100,
+        t: 100,
+        b: 100
+      }
+    };
+
+    // Render the plot to the div tag with id "bar", and pass barData and layout:
+    Plotly.newPlot("bar", barData, layout); 
+  }
+
+  // Function to make the bubble chart:
+  function makeBubbleChart(newdata) {
+    // Set all the vars in the array of sampleMatched (newdata) to object vars to build the charts:
+    var otu_ids = newdata.otu_ids;
+    var otu_labels = newdata.otu_labels;
+    var sample_values = newdata.sample_values;
+
+  }
+  
+
+  // Function to update the demographic panel with the chosen metadata's new info:
   function updatePanel(newdata) {
     // Set the div of id="sample-metadata" to panel
-    let panel = d3.select("#sample-metadata");
+    var panel = d3.select("#sample-metadata");
 
-    // Clear previous selection:
+    // Clear previous selection in the panel:
     panel.html("");
 
     // function to capitalize first letter in panel key-value pairs:
@@ -74,9 +127,9 @@ d3.json(url).then(function(data) {
       return string.charAt(0).toUpperCase() + string.slice(1);
     }
   
-    // Add each key and value to a new header seperated by a colon:
+    // Add each key and value pair in the chonen metadata to a new header- h5 - seperated by a colon:
     Object.entries(newdata).forEach(([key, value]) => {
-      var key = capitalizeFirstLetter(key) 
+      var key = capitalizeFirstLetter(key);
       panel.append("h5").text(`${key}: ${value}`);    
     });    
   } 
@@ -86,6 +139,7 @@ d3.json(url).then(function(data) {
   //   Plotly.restyle("pie", "values", [newdata]);
   // }
 
+  // Run the functions to create the dropdown menu and initilize the charts:
   createDropDown(data);  
   init();
   console.log("Original Data: ",data);
